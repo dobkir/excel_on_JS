34e8fd803dda7363e963c96a7c3c55d52bd87742
dobkir <dobkir@gmail.com>
2021-01-26 22:43:33 +0400

    webpack config after babel

--- webpack.config.js
+++ webpack.config.js
@@ -38,4 +38,34 @@ module.exports = {
             filename: 'bundle.[hash].css'
         }),
     ],
+    module: {
+        rules: [
+            // {
+            //     test: /\.css$/i,
+            //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            // },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // // Creates `style` nodes from JS strings
+                    // "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ],
+    },
 }
