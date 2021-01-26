236f181be03f32d37c44c03d42728301fce91632
dobkir <dobkir@gmail.com>
2021-01-26 21:41:41 +0400

    webpack config before new CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
-const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        // new CopyPlugin({
+        //     patterns: [
+        //         // { from: "source", to: "dest" },
+        //         // { from: "other", to: "public" },
+
+        //     ],
+        // }),
+        new CleanWebpackPlugin(),
+    ],
 }
